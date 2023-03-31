5af3b07127d9c52a4a45f6ea4274157810676c83
ALValdiviesoo <ledorvaldivieso@protonmail.com>
2023-03-31 10:04:44 +0800

    Squashed commit of the following:
    commit efe6353d21c4fee92d504bcc80ebb61a2b8b498e
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Fri Mar 31 10:01:36 2023 +0800
    
        Edited README file
    
    commit 1a4ba64f114c6d7bd14e04891aae3a6326440644
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Fri Mar 31 10:00:11 2023 +0800
    
        Styled the UI
    
    commit d61289bbb38f8c2c9c894e484d9aaa307aa7f044
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Thu Mar 30 20:54:13 2023 +0800
    
        Cleaned the code format
    
    commit 4a1a5abd0f73bdb21d11be64c631672104e2b848
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Thu Mar 30 20:46:15 2023 +0800
    
        Added player and computer score output
    
    commit 8aef31b21bc5d1eae9771f12f8ee48bcbfedc3d7
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Thu Mar 30 16:47:27 2023 +0800
    
        Changed the output into the DOM
    
    commit 7d693502f2e92a7b6e8b9a636496c3b17bab80fa
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Thu Mar 30 16:34:55 2023 +0800
    
        Changed input to buttons
    
    commit cf8af071654bc44e604f27388c8c3b5aa813feea
    Author: ALValdiviesoo <ledorvaldivieso@protonmail.com>
    Date:   Thu Mar 30 16:22:14 2023 +0800
    
        Removed the game function

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const choices = ["rock", "paper", "scissors"];
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const result = document.querySelector(".outputs");
+const scores = document.querySelector(".playerScore");
+const compScore = document.querySelector('.computerScore');
+
+const resultText = document.createElement('h2');
+resultText.classList.add('resultText');
+
 let playerScore = 0;
 let computerScore = 0;
+
+const playerScoreText = document.createElement('span');
+playerScoreText.classList.add('playerScoreText');
+
+const computerScoreText = document.createElement('span');
+computerScoreText.classList.add('computerScoreText');
 
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 3);
@@ -9,61 +25,84 @@ function getComputerChoice() {
   return(choices[random]);
 }
 
+function checkScore() {
+  if (computerScore === 5) {
+    alert("COMPUTER STOLE THE GAME!!!");
+    window.location.reload();
+  } else if (playerScore === 5) {
+    alert("YOU WIN THE MATCH!!!");
+    window.location.reload();
+  }
+}
+
+function playerWinsRound() {
+  result.appendChild(resultText);
+  playerScore++;
+  playerScoreText.textContent = playerScore;
+  scores.appendChild(playerScoreText);
+}
+
+function computerWinsRound() {
+  result.appendChild(resultText);
+  computerScore++;
+  computerScoreText.textContent = computerScore;
+  compScore.appendChild(computerScoreText);
+}
+
 function playRound(playerSelection, computerSelection) {
   switch(playerSelection.toUpperCase()) {
     case "ROCK":
       if (computerSelection.toUpperCase() === "ROCK") {
-        console.log("It's a tie!");
+        resultText.textContent = "It's a tie!";
+        result.appendChild(resultText);
       } else if (computerSelection.toUpperCase() === "PAPER") {
-        console.log("Computer wins! Paper beats rock!");
-        computerScore++;
+        resultText.textContent = "Computer wins! Paper beats rock!";
+        computerWinsRound();
       } else {
-        console.log("You win! Rock beats scissors!");
-        playerScore++;
+        resultText.textContent = "You win! Rock beats scissors!";
+        playerWinsRound();
       }
+      checkScore();
       break;
 
     case "PAPER":
       if (computerSelection.toUpperCase() === "PAPER") {
-        console.log("It's a tie!");
+        resultText.textContent = "It's a tie!";
+        result.appendChild(resultText);
       } else if (computerSelection.toUpperCase() === "ROCK") {
-        console.log("You win! Paper beats rock!");
-        playerScore++;
+        resultText.textContent = "You win! Paper beats rock!";
+        playerWinsRound();
       } else {
-        console.log("Computer wins! Scissors beats paper!");
-        computerScore++;
+        resultText.textContent = "Computer wins! Scissors beats paper!";
+        computerWinsRound();
       }
+      checkScore();
       break;
 
     case "SCISSORS":
       if (computerSelection.toUpperCase() === "SCISSORS") {
-        console.log("It's a tie!");
+        resultText.textContent = "It's a tie!";
+        result.appendChild(resultText);
       } else if (computerSelection.toUpperCase() === "ROCK") {
-        console.log("Computer wins! Rock beats scissors!");
-        computerScore++;
+        resultText.textContent = "Computer wins! Rock beats scissors!";
+        computerWinsRound();
       } else {
-        console.log("You win! Scissors beats paper!");
-        playerScore++;
+        resultText.textContent = "You win! Scissors beats paper!";
+        playerWinsRound();
       }
+      checkScore();
       break;
   }
 }
 
-function game() {
-  for(let i = 1; i <= 5; i++) {
-    const playerChoice = prompt("Rock, paper, or scissors? ");
-    const computerChoice = getComputerChoice();
-
-    playRound(playerChoice, computerChoice);
-  }
-
-  if (playerScore > computerScore) {
-    console.log("You win the game!!!");
-  } else if (computerScore > playerScore) {
-    console.log("Computer stole the game!!!") 
-  } else {
-    console.log("It's a tie!!??");
-  }
+rock.onclick = () => {
+  playRound("rock", getComputerChoice());
 }
 
-game();
+paper.onclick = () => {
+  playRound("paper", getComputerChoice());
+}
+
+scissors.onclick = () => {
+  playRound("scissors", getComputerChoice());
+}
